0c6690aad670361d48f83313d7711cc887b27db0
Henning Summer <115693958+henning-nr@users.noreply.github.com>
2023-11-30 01:31:01 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false,
-    firebaseConfig: {
-        apiKey: "AIzaSyD10djYDrgC5FMNHWtZ0QgK6SIJjG8SXtY",
-        authDomain: "crud-devops-74ea5.firebaseapp.com",
-        projectId: "crud-devops-74ea5",
-        storageBucket: "crud-devops-74ea5.appspot.com",
-        messagingSenderId: "292711652242",
-        appId: "1:292711652242:web:87385c358cd95640a43f4b",
-        measurementId: "G-C3M3BYCBM9"
-    }
+ 
   };
 
 
