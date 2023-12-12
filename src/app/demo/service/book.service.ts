c26adfb7c2d5078051d9e38974687244132e76a8
Guilherme Firpo <45392577+VailedFeline@users.noreply.github.com>
2023-12-11 22:49:28 -0300

    Add files via upload

---
+++ src/app/demo/service/book.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { book } from '../api/book.model';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class bookService {
+    private basePath = "products"
+
+    constructor(private db: AngularFireDatabase) { }
+
+    createBook(book: Book): any {
+        return this.db.list<Book>(this.basePath).push(book);
+    }
+
+    getBooks() {
+        return this.db.list<Book>(this.basePath).snapshotChanges().pipe(
+            map(changes =>
+                changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+            )
+        )
+    }
+
+    getBookId(key: string): Observable<Book> {
+        return this.db.object<Book>(`${this.basePath}/${key}`).valueChanges();
+    }
+
+    updateBook(key: string, value: any): Promise<void> {
+        return this.db.object<Book>(`${this.basePath}/${key}`).update(value);
+    }
+
+    deleteBook(key: string): Promise<void> {
+        return this.db.object<Book>(`${this.basePath}/${key}`).remove();
+    }
+}
