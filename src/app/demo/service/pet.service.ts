77313e6d5ff1557b20f0c1654a2ae686d42fcfa4
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-12-13 15:03:36 -0300

    add try/catch and await/async in service

--- src/app/demo/service/pet.service.ts
+++ src/app/demo/service/pet.service.ts
@@ -1,38 +1,51 @@
 import { Injectable } from '@angular/core';
-import { Pet } from '../api/pet.model';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Pet } from '../api/pet.model';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class PetService {
-    private basePath = "products"
+  private basePath = 'products';
 
-    constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase) {}
 
-    createPet(pet: Pet): any {
-        return this.db.list<Pet>(this.basePath).push(pet);
-    }
+  createPet(pet: Pet): Promise<any> {
+    return this.db.list<Pet>(this.basePath).push(pet).key;
+  }
 
-    getPets() {
-        return this.db.list<Pet>(this.basePath).snapshotChanges().pipe(
-            map(changes =>
-                changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
-            )
+  getPets(): Observable<Pet[]> {
+    return this.db
+      .list<Pet>(this.basePath)
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
         )
-    }
+      );
+  }
 
-    getPetId(key: string): Observable<Pet> {
-        return this.db.object<Pet>(`${this.basePath}/${key}`).valueChanges();
-    }
+  getPetById(key: string): Observable<Pet> {
+    return this.db.object<Pet>(`${this.basePath}/${key}`).valueChanges();
+  }
 
-    updatePet(key: string, value: any): Promise<void> {
-        return this.db.object<Pet>(`${this.basePath}/${key}`).update(value);
+  async updatePet(key: string, value: any): Promise<void> {
+    try {
+      await this.db.object<Pet>(`${this.basePath}/${key}`).update(value);
+    } catch (error) {
+      console.error('Erro ao atualizar o pet:', error);
+      throw error; // lança o erro novamente para que o chamador possa lidar com ele
     }
+  }
 
-    deletePet(key: string): Promise<void> {
-        return this.db.object<Pet>(`${this.basePath}/${key}`).remove();
+  async deletePet(key: string): Promise<void> {
+    try {
+      await this.db.object<Pet>(`${this.basePath}/${key}`).remove();
+    } catch (error) {
+      console.error('Erro ao excluir o pet:', error);
+      throw error; // lança o erro novamente para que o chamador possa lidar com ele
     }
+  }
 }
